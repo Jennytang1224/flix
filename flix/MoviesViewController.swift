401a597cfb631a255d2883b529c5b507100d25e9
Jenny Tang <tangjenny1224@gmail.com>
2021-02-06 14:52:34 -0800

    adding Podfile with AlamofireImage library

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -57,6 +58,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        cell.posterView.af.setImage(withURL: posterUrl!)
         return cell
     }
 
