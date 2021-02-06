d35ec69ce919868eff2dae27f183ff03aff65b40
Jenny Tang <tangjenny1224@gmail.com>
2021-02-06 14:04:22 -0800

    first commit. need to add pics

---
+++ flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Jenny Tang on 2/3/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
